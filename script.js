11eec3816f792893bf54e88e825ec2f4baacdc40
bwettenstein <wettenstein@gmail.com>
2020-02-10 22:54:13 -0800

    Began scripting buttons and changed button colors

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function add(num1, num2) {
-    return num1+num2;
+const keys = document.querySelectorAll('.calc-keys');
+let outputMessage = document.querySelector('.output-display');
+const defaultMessage = 0;
+outputMessage.textContent = defaultMessage;
+
+console.log(keys)
+let add = (num1, num2) => {
+    a+b;
 }
-function subtract(num1, num2) {
-    return num1 - num2;
+let subtract = (num1, num2) => {
+    num1 - num2;
 }
