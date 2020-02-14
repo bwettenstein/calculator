a4c690f573edb25178f3675e687a34eb66b5d8e8
bwettenstein <wettenstein@gmail.com>
2020-02-13 19:38:09 -0800

    Redid styling using grid instead of flexbox

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let outputMessage = document.querySelector('.output-display');
+let numbersButton = document.querySelector('.numbers-container');
+let clear = document.querySelector('.clear')
+clear.addEventListener('click', () => output.textContent = "");
 const defaultMessage = 0;
 let previousNum = defaultMessage;
 let currentNum = defaultMessage;
 
 outputMessage.textContent = defaultMessage;
 
-console.log(keys)
+numbersButton.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+        console.log(this.id)
+    }
+})
+
 let add = (num1, num2) => {
     a+b;
 }
@@ -14,11 +22,9 @@ let subtract = (num1, num2) => {
 }
 
 function printValue() {
-    console.log(this.value);
+    console.log(this.id);
 }
+
 // HAVE A FUNCTION CALL OTHER FUNCTIONS BASED ON THE CLASS NAME
-
-
-
 
 
