3a30271f14aa22c4d325938db70e9d0e6319906b
bwettenstein <wettenstein@gmail.com>
2020-02-22 00:12:28 -0800

    Calculator will now calculate

--- script.js
+++ script.js
@@ -12,23 +12,55 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOutput = this.currentOutput.toString().slice(0, -1);
     }
 
     replaceNumber(number) {
-        this.currentOutput = number;
+        // Prevent decimals from being displayed multiple times
+        if (number === '.' && this.currentOutput.includes('.')) return
+        this.currentOutput = this.currentOutput.toString() + number.toString();
     }
 
     chooseOperation(operation) {
-
+        if (this.currentOutput === '') return
+        if (this.previousOutput !== '') {
+            this.calculate()
+        }
+        this.operation = operation;
+        this.previousOutput = this.currentOutput;
+        this.currentOutput = '';
     }
 
     calculate() {
-
+        let computation
+        // Converts the previousOutput, which is a string, to a floating point
+        const prev = parseFloat(this.previousOutput);
+        const current = parseFloat(this.currentOutput);
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+': 
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case 'x':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOutput = computation;
+        this.operation = undefined;
+        this.previousOutput = '';
     }
 
     updateDisplay() {
         this.currentOutputText.innerText = this.currentOutput;
+        this.previousOutputText.innerText = this.previousOutput;
     }
 }
 
@@ -50,5 +82,24 @@ numberButtons.forEach(button => {
     })
 })
 
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
 
+equalsButton.addEventListener('click', button => {
+    calculator.calculate();
+    calculator.updateDisplay();
+})
 
+clearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
